2e952c17f5542ad7dc7f67cf0ddd8c4d57b97519
Maksim Borisov <Lixard870@gmail.com>
2020-06-01 04:59:58 +0300

    Add test toolbar, remove unnecessary resources

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,18 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule, MatCardModule]
+  imports: [
+    CommonModule,
+    SharedModule,
+    HomeRoutingModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule
+  ]
 })
 export class HomeModule {}
