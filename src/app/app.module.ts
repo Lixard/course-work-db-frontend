fa02f5394ce632c6caaf0c0d65082296e2714ea5
Maksim Borisov <Lixard870@gmail.com>
2020-06-02 18:27:31 +0300

    Add created modules to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import 'reflect-metadata';
 import '../polyfills';
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { CoreModule } from './core/core.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {CoreModule} from './core/core.module';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
 
-import { HomeModule } from './pages/home-page/home.module';
+import {HomeModule} from './pages/home-page/home.module';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppointmentPagesModule} from "./pages/appointment-pages/appointment-pages.module";
+import {DiagnosisPagesModule} from "./pages/diagnosis-pages/diagnosis-pages.module";
+import {DrugPagesModule} from "./pages/drug-pages/drug-pages.module";
+import {PatientPagesModule} from "./pages/patient-pages/patient-pages.module";
+import {DoctorPagesModule} from "./pages/doctor-pages/doctor-pages.module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +28,11 @@ import { AppComponent } from './app.component';
     CoreModule,
     HomeModule,
     AppRoutingModule,
+    AppointmentPagesModule,
+    DiagnosisPagesModule,
+    DoctorPagesModule,
+    DrugPagesModule,
+    PatientPagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
