adadb7a386a71b6a162ad5f4428fda278126be57
Maksim Borisov <Lixard870@gmail.com>
2020-06-06 10:12:42 +0300

    Add search to appointment-list page

--- src/app/pages/appointment-pages/appointment-list/appointment-list.component.ts
+++ src/app/pages/appointment-pages/appointment-list/appointment-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AppointmentService} from "../../../core/services/appointment.service";
 import {Appointment} from "../../../core/models/appointment.model";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-appointment-list',
@@ -9,14 +10,20 @@ import {Appointment} from "../../../core/models/appointment.model";
 })
 export class AppointmentListComponent implements OnInit {
 
-  dataSource: Appointment[];
+  dataSource: MatTableDataSource<Appointment>;
   displayColumns: string[] = ['appointmentId', 'patientId', 'doctorId', 'place', 'appointmentDate', 'symptoms'];
 
   constructor(private appointmentService: AppointmentService) {
   }
 
   ngOnInit(): void {
-    this.appointmentService.getAppointments().subscribe(value => this.dataSource = value);
+    this.appointmentService.getAppointments().subscribe(
+      values => this.dataSource = new MatTableDataSource<Appointment>(values)
+    );
   }
 
+  applyFilter($event: KeyboardEvent) {
+    const filterValue = ($event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
